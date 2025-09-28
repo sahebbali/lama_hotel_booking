3e3299ae102274759f405fa5845b89a8022c8305
sahebbali <saheb1996jmr@gmail.com>
2025-09-28 17:25:29 +0600

    fixed some issue

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,5 +1,6 @@
 import useFetch from "../../hooks/useFetch";
 import "./featured.css";
+import berlinImage from "../../assets/hotel/berlin.jpg";
 
 const Featured = () => {
   const { data, loading, error } = useFetch(
@@ -13,11 +14,7 @@ const Featured = () => {
       ) : (
         <>
           <div className="featuredItem">
-            <img
-              src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
-              alt=""
-              className="featuredImg"
-            />
+            <img src={berlinImage} alt="berlin" className="featuredImg" />
             <div className="featuredTitles">
               <h1>Berlin</h1>
               <h2>{data[0]} properties</h2>
